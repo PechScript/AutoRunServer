9d2e7e37d4ff6b8b2924bd53b2a4b5755ee8cde9
PechScript <90135306+PechScript@users.noreply.github.com>
2023-08-07 07:47:58 -0400

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "================================================="
-echo -e "#       Premium Auto Script By PechScript       #"
+echo -e "#       Premium Auto Script By Pech Chhean      #"
 echo -e "================================================="
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
